0bed35f6009a05a19dcf8bff15ef930fe00d360b
GitIonahu <playsky.xrhu@gmail.com>
2022-02-11 06:06:21 +0800

    add Readme.txt

---
+++ contoursDetection.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3,frameWidth)
+cap.set(4,frameHeight)
+
+while True:
+    success,img = cap.read()
+
+    imgBlur = cv2.GaussianBlur(img,(7,7),1)
+    imgGray  = cv2.cvtColor(imgBlur,cv2.COLOR_BGR2GRAY)
+
+
+
+    cv2.imshow("Result",img)
+    if cv2.waitKey(1) & 0xFF == ord('q')
+        break
